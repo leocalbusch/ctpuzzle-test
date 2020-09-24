585eb68714bc10da430602756264c331da13cb03
Leonardo Calbusch <leonardo@calbusch.com.br>
2020-09-24 14:17:04 -0300

    Ajustes de interface

--- dashboard.php
+++ dashboard.php
@@ -73,6 +73,12 @@ require "conexao.php";
         .comic{
             font-family: "Comic Sans MS";
         }
+        .canvaBoy{
+            background: #dbf4d3;
+        }
+        .canvaGirl{
+            background: lavenderblush;
+        }
     </style>
     <title>CT Puzzle Test</title>
 </head>
@@ -112,39 +118,39 @@ require "conexao.php";
         <div class="col-lg-6 col-sm-10">
             <?php
             if ($_SESSION["tipoUsuario"] == 3) { ?>
-                <div class="card d-flex flex-row">
+                <div class="card d-flex flex-row canvaBoy">
                     <img src="img/Instrucoes/guias/boy0000.png" class="w-25 h-25" alt="">
                     <div class="card-body" style="background:url(img/Instrucoes/guias/chat_bubble_left.png) no-repeat; background-size:100% 100%;">
                         <p class="card-text pl-5"><strong>Olá <?php echo $_SESSION[nome]; ?>!</strong> Bem-vindo(a) ao CT Puzzle Test, um teste sobre Pensamento Computacional! Antes de começar, temos algumas instruções importantes pra você. <strong>Preste bastante atenção!</strong></p>
                     </div>
                 </div>
-                <div class="card d-flex flex-row">
+                <div class="card d-flex flex-row canvaGirl">
                     <div class="card-body" style="background:url(img/Instrucoes/guias/chat_bubble_right.png) no-repeat; background-size:100% 100%;">
                         <p class="card-text pr-5">O objetivo é chegar ao final sem se preocupar com uma pontuação. Você vai encontrar vários tipos de desafios. Para resolvê-los, você vai precisar clicar em objetos, arrastar figuras pela tela, girar e trocar coisas de lugar.</p>
                     </div>
                     <img src="img/Instrucoes/guias/girl0000.png" class="w-25 h-25" alt="">
                 </div>
-                <div class="card d-flex flex-row">
+                <div class="card d-flex flex-row canvaBoy">
                     <img src="img/Instrucoes/guias/boy0000.png" class="w-25 h-25" alt="">
                     <div class="card-body" style="background:url(img/Instrucoes/guias/chat_bubble_left.png) no-repeat; background-size:100% 100%;">
                         <p class="card-text pl-5">Para se dar bem no teste, tente resolvê-lo no menor tempo possível. Além disso, tente sempre encontrar a melhor solução para cada desafio.</p>
                     </div>
                 </div>
-                <div class="card d-flex flex-row">
+                <div class="card d-flex flex-row canvaGirl">
                     <div class="card-body" style="background:url(img/Instrucoes/guias/chat_bubble_right.png) no-repeat; background-size:100% 100%;">
                         <p class="card-text pr-5">Procure usar a menor quantidade de recursos possível, tentando dar menos cliques na tela, menos giros nos objetos, menos comandos nos programas. Você pode pular se estiver muito difícil, mas não desista! Mostre que você consegue!</p>
                     </div>
                     <img src="img/Instrucoes/guias/girl0000.png" class="w-25 h-25" alt="">
                 </div>
-                <div class="card d-flex flex-row">
+                <div class="card d-flex flex-row canvaBoy">
                     <img src="img/Instrucoes/guias/boy0000.png" class="w-25 h-25" alt="">
                     <div class="card-body" style="background:url(img/Instrucoes/guias/chat_bubble_left.png) no-repeat; background-size:100% 100%;">
                         <p class="card-text pl-5">Algumas fases possuem dicas que te mostram o próximo passo para resolver o desafio. Tente resolver sem usar dicas, mas se não conseguir avançar, elas estão ali pra te ajudar.</p>
                     </div>
                 </div>
-                <div class="card d-flex flex-row">
-                    <div class="card-body ml-5" style="background:url(img/Instrucoes/guias/chat_bubble_right.png) no-repeat; background-size:100% 100%;">
-                        <p class="card-text pr-5">Para iniciar seu teste, você precisa informar aqui na caixinha a sua chave de acesso! Ainda não possui uma ou não sabe do que eu tô falando? Pergunta pra pessoa responsável pela aplicacão do teste, provavelmente seu professor! Vamos lá?</p>
+                <div class="card d-flex flex-row canvaGirl">
+                    <div class="card-body" style="background:url(img/Instrucoes/guias/chat_bubble_right.png) no-repeat; background-size:100% 100%;">
+                        <p class="card-text pr-5">Para iniciar seu teste, você precisa informar aqui na caixinha a sua chave de acesso! Ainda não possui uma ou não sabe do que eu estou falando? Pergunta pra pessoa responsável pela aplicacão do teste, provavelmente seu professor! Vamos lá?</p>
                     </div>
                     <img src="img/Instrucoes/guias/girl0000.png" class="w-25 h-25" alt="">
                 </div>
@@ -153,7 +159,7 @@ require "conexao.php";
                     <div class="card-body">
                         <form action='processaVinculaAmostra.php' method='post' >
                             <div class='form-group'>
-                                <label for='chaveAmostra'>Chave:</label>
+                                <label for='chaveAmostra'>Chave de acesso:</label>
                                 <input type='text' class='form-control' id='chaveAmostra' name='chaveAmostra'><br/>
                                 <button type='submit' class='btn btn-primary'>Iniciar</button>
                             </div>
