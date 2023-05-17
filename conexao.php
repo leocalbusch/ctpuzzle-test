e28da88f6aae712f79be34ba7b5a767c58fa1b9f
Leonardo <leonardo@calbusch.com.br>
2023-05-17 10:21:42 -0300

    teste

--- conexao.php
+++ conexao.php
@@ -1,1 +1,9 @@
 
+<?php
+$conexao = mysqli_connect('lite.acad.univali.br', 'leo', '2#KTFA2Nz@SzrTxv', 'ct_puzzle', '443');
+if (!$conexao) {
+    $erro = "Ocorreu um erro na conexão com o banco de dados. Por favor, realize login novamente.";
+    require "modalErro.php";
+    mysqli_close($conexao);
+    exit();
+}
